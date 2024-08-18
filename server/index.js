1fe404a628cfbee17b63ea960f85a5dbf1ea92f1
Prajwal Negi <166615651+prajwalnegi2301@users.noreply.github.com>
2024-08-18 16:40:49 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -41,7 +41,7 @@ app.use(compression({
 dotenv.config();
 
 app.use(cors({
-    origin: [process.env.FRONTEND_URL,process.env.DASHBOARD_URL],
+    origin: "*",
     methods: ["GET","POST","PUT","DELETE"],
     credentials:true
 }));
